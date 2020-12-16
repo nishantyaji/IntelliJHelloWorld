8a1fc9e3fbc5584144afb660c0926edc8c3ac31e
nishantyaji <nishant.yaji@gmail.com>
2020-12-16 16:10:39 +0530

    New solution to a hacker rank problem

--- src/com/hackerrank/IceCreamParlour.java
+++ src/com/hackerrank/IceCreamParlour.java
@@ -1,43 +1,41 @@
 package com.hackerrank;
 
-import java.util.HashSet;
 import java.util.Hashtable;
-import java.util.Set;
 
 public class IceCreamParlour {
     public static void main(String[] args) {
-        whatFlavors(new int [] {4, 3, 2, 5, 7}, 8);
+        whatFlavors(new int[]{4, 3, 2, 5, 7}, 8);
     }
 
     // Complete the whatFlavors function below.
     static void whatFlavors(int[] cost, int money) {
         Hashtable<Integer, Integer> allElements = new Hashtable<>();
         for (int i1 : cost) {
-            if(allElements.containsKey(i1)) {
+            if (allElements.containsKey(i1)) {
                 int count = allElements.get(i1);
-                allElements.put(i1, count+1);
-            } else{
+                allElements.put(i1, count + 1);
+            } else {
                 allElements.put(i1, 1);
             }
         }
         int firstIndex = 0;
         int secondIndex = 0;
-        for(int i = 0; i < cost.length; i++) {
-            if(allElements.containsKey(money - cost[i])) {
-                if(money - cost[i] != cost[i]) {
+        for (int i = 0; i < cost.length; i++) {
+            if (allElements.containsKey(money - cost[i])) {
+                if (money - cost[i] != cost[i]) {
                     firstIndex = i + 1;
                     break;
                 } else {
-                    if(allElements.get(cost[i]) > 1) {
+                    if (allElements.get(cost[i]) > 1) {
                         firstIndex = i + 1;
                         break;
                     }
                 }
             }
         }
-        for(int j = firstIndex; j < cost.length; j++) {
-            if(cost[j] == money - cost[firstIndex-1]) {
-                secondIndex = j+1;
+        for (int j = firstIndex; j < cost.length; j++) {
+            if (cost[j] == money - cost[firstIndex - 1]) {
+                secondIndex = j + 1;
                 break;
             }
         }
